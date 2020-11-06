248393cd9be5774dc09dea40451885707de6b6e1
James Barlow <tentbub@Jamess-MacBook-Pro.local>
2020-11-06 00:01:44 -0700

    message

---
+++ app.js
@@ -0,0 +1,9 @@
+$('.navItem').css('color', 'white');
+
+$('#nav').css('background-color','#145')
+
+$('li:even').css('background-color','#ccc')
+
+$('li:odd').css('backgoround-color', '#aaa')
+
+$('input').val("James")
